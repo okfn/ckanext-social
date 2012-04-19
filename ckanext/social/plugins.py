987ec65da9e286cd6bc5810d5edad8e879768b96
Toby <toby.junk@gmail.com>
2012-04-19 12:27:49 +0100

    switch to use plugins.toolkit

--- ckanext/social/plugins.py
+++ ckanext/social/plugins.py
@@ -1,10 +1,4 @@
-import os
-from webhelpers.html.tags import literal
-from paste.deploy.converters import asbool
-
-import ckan.plugins as plugins
-import ckan.lib.base as base
-import ckanext.social
+import ckan.plugins as p
 
 available_sites = {'sharethis' : 'ShareThis',
                    'twitter' : 'Tweet',
@@ -17,38 +11,31 @@ styles = {'horizontal' : '_hcount',
           'none' : '',
           'large' : '_large'}
 
-def configure_template_directory(config, relative_path):
-    configure_served_directory(config, relative_path, 'extra_template_paths')
 
-def configure_public_directory(config, relative_path):
-    configure_served_directory(config, relative_path, 'extra_public_paths')
+class Social(p.SingletonPlugin):
 
-def configure_served_directory(config, relative_path, config_var):
-    'Configure serving of public/template directories.'
-    assert config_var in ('extra_template_paths', 'extra_public_paths')
-    this_dir = os.path.dirname(ckanext.social.__file__)
-    absolute_path = os.path.join(this_dir, relative_path)
-    if absolute_path not in config.get(config_var, ''):
-        if config.get(config_var):
-            config[config_var] += ',' + absolute_path
-        else:
-            config[config_var] = absolute_path
-
-
-class Social(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IConfigurable)
-    plugins.implements(plugins.ITemplateHelpers)
+    p.implements(p.IConfigurer)
+    p.implements(p.IConfigurable)
+    p.implements(p.ITemplateHelpers)
 
     def configure(self, config):
-        self.__class__.config = config
-        self.__class__.sites = config.get('social.sharethis_sites','sharethis').split()
         style = config.get('social.sharethis_style', 'horizontal')
-        self.__class__.style = styles[style]
+        style = styles.get(style, '')
+
+        sites = config.get('social.sharethis_sites','sharethis')
+        sites = p.toolkit.aslist(sites)
+
+        multipost = config.get('social.sharethis_multipost', 'true')
+        multipost = p.toolkit.asbool(multipost)
+
+        # store these so available to class methods
+        self.__class__.style = style
+        self.__class__.sites = sites
+        self.__class__.multipost = multipost
 
     def update_config(self, config):
-        configure_template_directory(config, 'templates')
-        #configure_public_directory(config, 'public')
+        # add template directory
+        p.toolkit.add_template_directory(config, 'templates')
 
     @classmethod
     def sharethis(cls, style=None, sites=None, multipost=None):
@@ -65,7 +52,7 @@ class Social(plugins.SingletonPlugin):
 
         # override default multipost
         if multipost is None:
-            multipost = asbool(cls.config.get('social.sharethis_multipost', 'true'))
+            multipost = cls.multipost
 
         links = []
         for site in cls.sites:
@@ -73,9 +60,9 @@ class Social(plugins.SingletonPlugin):
                 data = (site, style, available_sites[site])
                 links.append('<span class="st_%s%s" displayText="%s"></span>' % data)
 
-        buttons = literal('\n'.join(links))
+        buttons = p.toolkit.literal('\n'.join(links))
         data = {'buttons' : buttons, 'switchTo5x' : multipost}
-        return literal(base.render('sharethis.html', data))
+        return p.toolkit.render_snippet('sharethis.html', data)
 
     def get_helpers(self):
         return {'social_sharethis': self.sharethis}
